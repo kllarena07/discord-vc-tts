089c15fd57a709f54dfda098c4fcd2fb7e520d09
Kieran Llarena <baymax709@gmail.com>
2025-08-25 14:35:29 -0400

    up-to-date

--- bot.js
+++ bot.js
@@ -1,17 +1,22 @@
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
-const axios = require('axios');
-const { Readable } = require('stream');
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  getVoiceConnection,
+} = require("@discordjs/voice");
+const axios = require("axios");
+const { Readable } = require("stream");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
@@ -24,18 +29,20 @@ const SIMILARITY_BOOST = process.env.SIMILARITY_BOOST;
 
 let isListening = false;
 
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity('messages', { type: ActivityType.Listening });
+  client.user.setActivity("messages", { type: ActivityType.Listening });
 });
 
-client.on('messageCreate', async message => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
-  if (message.content === '!join' && message.author.id === ALLOWED_USER_ID) {
+  if (message.content === "!join" && message.author.id === ALLOWED_USER_ID) {
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
-      return message.reply('You need to be in a voice channel to use this command.');
+      return message.reply(
+        "You need to be in a voice channel to use this command.",
+      );
     }
     await joinVoiceChannel({
       channelId: voiceChannel.id,
@@ -43,54 +50,58 @@ client.on('messageCreate', async message => {
       adapterCreator: voiceChannel.guild.voiceAdapterCreator,
     });
     isListening = true;
-    message.reply('Joined the voice channel and started listening!');
+    message.reply("Joined the voice channel and started listening!");
   }
 
-  if (message.content === '!scream' && message.author.id === ALLOWED_USER_ID) {
+  if (message.content === "!scream" && message.author.id === ALLOWED_USER_ID) {
     const connection = getVoiceConnection(message.guild.id);
     if (connection) {
-      const resource = createAudioResource('smack_talk.mp3');
+      const resource = createAudioResource("scream.wav");
       const player = createAudioPlayer();
       connection.subscribe(player);
       player.play(resource);
-      message.reply('Playing scream sound!');
+      message.reply("Playing scream sound!");
     } else {
-      message.reply('I need to be in a voice channel first. Use !join');
+      message.reply("I need to be in a voice channel first. Use !join");
     }
   }
 
-  if (message.content === '!leave' && message.author.id === ALLOWED_USER_ID) {
+  if (message.content === "!leave" && message.author.id === ALLOWED_USER_ID) {
     const connection = getVoiceConnection(message.guild.id);
     if (connection) {
       connection.destroy();
       isListening = false;
-      message.reply('Left the voice channel and stopped listening!');
+      message.reply("Left the voice channel and stopped listening!");
     } else {
-      message.reply('I\'m not in a voice channel.');
+      message.reply("I'm not in a voice channel.");
     }
   }
 
-  if (isListening && message.author.id === ALLOWED_USER_ID && message.content !== "!scream") {
+  if (
+    isListening &&
+    message.author.id === ALLOWED_USER_ID &&
+    message.content !== "!scream"
+  ) {
     const connection = getVoiceConnection(message.guild.id);
     if (connection) {
       try {
         const response = await axios({
-          method: 'POST',
+          method: "POST",
           url: `https://api.elevenlabs.io/v1/text-to-speech/${ELEVENLABS_VOICE_ID}`,
           headers: {
-            'Accept': 'audio/mpeg',
-            'xi-api-key': ELEVENLABS_API_KEY,
-            'Content-Type': 'application/json'
+            Accept: "audio/mpeg",
+            "xi-api-key": ELEVENLABS_API_KEY,
+            "Content-Type": "application/json",
           },
           data: {
             text: message.content,
             model_id: ELEVENLABS_MODEL_ID,
             voice_settings: {
               stability: STABILITY,
-              similarity_boost: SIMILARITY_BOOST
-            }
+              similarity_boost: SIMILARITY_BOOST,
+            },
           },
-          responseType: 'arraybuffer'
+          responseType: "arraybuffer",
         });
 
         const audioBuffer = Buffer.from(response.data);
@@ -102,8 +113,8 @@ client.on('messageCreate', async message => {
 
         console.log(`Speaking: ${message.content}`);
       } catch (error) {
-        console.error('Error generating speech:', error);
-        message.reply('An error occurred while generating speech.');
+        console.error("Error generating speech:", error);
+        message.reply("An error occurred while generating speech.");
       }
     }
   }
